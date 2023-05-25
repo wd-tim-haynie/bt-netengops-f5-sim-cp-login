492e716bbdb3c76f0d971ee0f6128f6bddb7790c
wd-tim-haynie <129900794+wd-tim-haynie@users.noreply.github.com>
2023-05-25 14:57:19 -0700

    Update sim_cp_login.sh
    version 1.3

--- sim_cp_login.sh
+++ sim_cp_login.sh
@@ -1,103 +1,101 @@
 #!/bin/bash
-# Version 1.2.1
+
+#sim-cp-login.sh
+#Version 1.3 May 25, 2023
+#https://github.com/wd-tim-haynie/bt-netengops-f5-sim-cp-login
+#Author: Tim Haynie, CWNE #254, ACMX #508 https://www.linkedin.com/in/timhaynie/
 
 # Note the start time
 START_TIME=$(date +%s%N)
 
-# Uncomment the following lines for testing
+# Remove “::ffff:” from the IP passed as the first argument. Compatible with both IPv4 and IPv6, unless your IPv6 IP includes ::ffff:
+IP=$(echo ${1} | sed 's/::ffff://')
+PORT=${2}  # The port number is the second argument
+
+# Uncomment the following lines for testing at CLI
 #USERNAME='test-username'
 #PASSWORD='test-password'
 #ENCRYPTED_PASSWORD="U2FsdGVkX19tKPeA3hWRZcfGl6H+ILXks2VtidUEbb7unSw7NAzzIg/OYWl+Myx8"
+#LOG_LEVEL="err"
+#DECRYPTION_KEY_FILE_NAME="sim_cp_login.key"
 
-# Remove “::ffff:” from the IP passed as the first argument
-IP=${1:7}  # This is fast but not applicable to IPv6 or for testing at CLI
-# Uncomment the following line for IPv6 or for testing IPv4 at CLI
-#IP=$(echo ${1} | sed 's/::ffff://')
-
-PORT=${2}  # The port number is the second argument
-
-# Enable or disable logging
-#LOGGING=true # true or false to turn logging on/off
+# Logging section
 # Logging level hierarchy (in order of severity)
-declare -A LOG_LEVELS=( ["debug"]=0 ["info"]=1 ["notice"]=2 ["warn"]=3 ["err"]=4 ["crit"]=5 ["alert"]=6 ["emerg"]=7 )
-# Minimum logging level
-#MIN_LOG_LEVEL="err"
-
-ALL_LOG_LEVELS=$(echo ${!LOG_LEVELS[@]})
-
-# If MIN_LOG_LEVEL is not set or is not a valid log level, default it to "warn"
-if [[ -z "${MIN_LOG_LEVEL}" || -z "${LOG_LEVELS[$MIN_LOG_LEVEL]}" ]]
-then
-    if [[ "$LOGGING" == "true" ]]
-    then
-        MIN_LOG_LEVEL="warn"
-        echo "Invalid MIN_LOG_LEVEL ${MIN_LOG_LEVEL} in $MON_TMPL_NAME, defaulting to warn" | logger -p local0.warn > /dev/null 2>&1
-
-    fi
-else
-    # If MIN_LOG_LEVEL is set to a valid value and LOGGING is null from environment, enable logging
-    if [[ -z "$LOGGING" ]]
-    then
-        LOGGING="true"
-    fi
-fi
+declare -A ALL_LOG_LEVELS=( ["debug"]=0 ["info"]=1 ["notice"]=2 ["warn"]=3 ["err"]=4 ["crit"]=5 ["alert"]=6 ["emerg"]=7 )
+ALL_LOG_LEVELS_STR=$(echo ${!ALL_LOG_LEVELS[@]})
 
 LOG_MESSAGE() {
     # Check if provided log level is valid
-    if [[ -n "${LOG_LEVELS[$2]}" ]]
+    if [[ -n "${ALL_LOG_LEVELS[$2]}" ]]
     then
-        # Check if logging is enabled and its severity is greater or equal to MIN_LOG_LEVEL
-        if [[ $LOGGING == "true" && ${LOG_LEVELS[$2]} -ge ${LOG_LEVELS[$MIN_LOG_LEVEL]} ]]
+        # If LOG_LEVEL is set and its severity is less than or equal to the provided log level, log the message
+        if [[ -n "$LOG_LEVEL" && ${ALL_LOG_LEVELS[$2]} -ge ${ALL_LOG_LEVELS[$LOG_LEVEL]} ]]
         then
             # Log the message with the provided level
-            echo "$1" | logger -p local0.$2 > /dev/null 2>&1
+            echo "$MON_TMPL_NAME PID: $$ Name: $NODE_NAME IP: $IP Port: $PORT $1" | logger -p local0.$2 > /dev/null 2>&1
         fi
     else
-        # If the log level provided was invalid, log an error message
-        LOG_MESSAGE "Invalid log level provided: Monitor: $MON_TMPL_NAME Message: $1 Level $2. Valid log levels are: ${ALL_LOG_LEVELS}" "err"
+        # Log level is invalid, log a warning
+        LOG_MESSAGE "Invalid log level \"$2\" for provided log. Valid levels are: $ALL_LOG_LEVELS_STR. Message: $1" "warn"
     fi
 }
+
+# Validate LOG_LEVEL, unset to disable logging if it is invalid
+if [[ -z "$LOG_LEVEL" ]]
+then
+    : # Do nothing, it's already empty, logging is disabled
+elif [[ -z "${ALL_LOG_LEVELS[$LOG_LEVEL]}" ]]
+then
+    # LOG_LEVEL is either null or invalid, disable logging
+    PROVIDED_LOG_LEVEL=${LOG_LEVEL}
+    LOG_LEVEL="warn"
+    LOG_MESSAGE "Invalid LOG_LEVEL \"$PROVIDED_LOG_LEVEL\", disabling additional logging" "warn"
+    unset LOG_LEVEL
+fi
+
+# End of Logging section
 
 # Decrypt the password if ENCRYPTED_PASSWORD is not empty (-n)
 if [[ -n "$ENCRYPTED_PASSWORD" ]]
 then
 
     # Locate the key file. Use head to only take a single entry and discard any other matches.
-    DECRYPTION_FILE_PATH=$(ls /config/filestore/files_d/Common_d/ifile_d/:Common:sim_cp_login.key_* 2> /dev/null | head -n 1)
+    DECRYPTION_KEY_FILE_PATH=$(ls /config/filestore/files_d/Common_d/ifile_d/:Common:${DECRYPTION_KEY_FILE_NAME}_* 2> /dev/null | head -n 1)
 
     # Check to see if its not empty (-n) and a real file (-f)
-    if [[ -n "$DECRYPTION_FILE_PATH" && -f "$DECRYPTION_FILE_PATH" ]]
+    if [[ -n "$DECRYPTION_KEY_FILE_PATH" && -f "$DECRYPTION_KEY_FILE_PATH" ]]
     then
-        read -r DECRYPTION_KEY < "$DECRYPTION_FILE_PATH" 2> /dev/null
+        # Read the first line of the decryption key file
+        read -r DECRYPTION_KEY < "$DECRYPTION_KEY_FILE_PATH" 2> /dev/null
 
-        if ! [[ -n "$ENCRYPTED_PASSWORD" ]]
+        # Check to see if the key is empty
+        if [[ -z "$DECRYPTION_KEY" ]]
         then
-            LOG_MESSAGE "Decryption file exists but seems empty for $MON_TMPL_NAME Name: $NODE_NAME IP: $IP Port: $PORT" "err"
+            LOG_MESSAGE "Decryption file exists but seems empty" "err"
             exit 1
         fi
-    else
-        LOG_MESSAGE "Decryption file not found for $MON_TMPL_NAME Name: $NODE_NAME IP: $IP Port: $PORT" "err"
+    else # We couldn't find the key file
+        LOG_MESSAGE "Decryption file not found" "err"
         exit 1
     fi    
+
+    # Use openssl to decrypt the password
     PASSWORD=$(echo "$ENCRYPTED_PASSWORD" | openssl enc -aes-256-cbc -d -a -k "$DECRYPTION_KEY" 2> /dev/null)
-    if ! [[ -n "$PASSWORD" ]]
+    
+    # If there's a problem with openssl, then password will be blank.
+    if [[ -z "$PASSWORD" ]]
     then
-        LOG_MESSAGE "An error occurred with openssl for $MON_TMPL_NAME Name: $NODE_NAME IP: $IP Port: $PORT" "err"
+        LOG_MESSAGE "An error occurred with openssl" "err"
         exit 1
     fi
 fi
 
-# Define the URL
-URL_PREFIX="https://${IP}:${PORT}"
-URI="/guest/auth_login.php"
-URL="${URL_PREFIX}${URI}"
-
-# Remove old cookie files
-find /tmp -name "$(basename ${0}).${IP}_${PORT}_*.cookie" -type f -delete > /dev/null 2>&1
-
 # Define temporary files
-COOKIE_FILE="/tmp/$(basename ${0}).${IP}_${PORT}_$$.cookie"
-PID_FILE="/var/run/$(basename ${0}).${IP}_${PORT}.pid"
+COOKIE_FILE="/tmp/$(basename ${MON_TMPL_NAME})_${IP}_${PORT}_$$.cookie"
+PID_FILE="/var/run/$(basename ${MON_TMPL_NAME})_${IP}_${PORT}.pid"
+
+# Cleanup any previous cookie
+rm -f "/tmp/$(basename ${MON_TMPL_NAME}).${IP}_${PORT}_$(cat "$PID_FILE").cookie" > /dev/null 2>&1
 
 # Cleanup function to ensure removal of temporary files on script exit
 CLEANUP() {
@@ -106,22 +104,29 @@ CLEANUP() {
 }
 
 # Setup trap to call cleanup function on script exit
-trap cleanup EXIT INT TERM ERR
+trap CLEANUP EXIT INT TERM ERR
+
 
 # Kill the last instance of this monitor if it is hung, and log the current PID
 if [ -f "$PID_FILE" ]
 then
    PID=$(cat "$PID_FILE")
-   if kill -0 "$PID" 2>/dev/null; then
-       LOG_MESSAGE "EAV exceeded runtime, killing previous instance ${IP}:${PORT}" "err"
+   if kill -0 "$PID" 2>/dev/null
+   then
+       LOG_MESSAGE "EAV exceeded runtime, killing previous instance" "err"
        kill "$PID" > /dev/null 2>&1
    fi
 fi
 
-# Record the PID ($$) to the PID file
+# Set the PID file (overwrites previous value)
 echo "$$" > "$PID_FILE"
 
 # Begin the actual monitor
+# Define the URL
+URL_PREFIX="https://${IP}:${PORT}"
+URI="/guest/auth_login.php"
+URL="${URL_PREFIX}${URI}"
+
 # Get the initial cookie and ntok
 INIT_RESPONSE=$(curl -k -c "$COOKIE_FILE" -s -S "$URL")
 NTOK=$(echo "$INIT_RESPONSE" | grep -oP 'name="ntok" id="[^"]*" value="\K[^"]*')
@@ -129,12 +134,13 @@ NTOK=$(echo "$INIT_RESPONSE" | grep -oP 'name="ntok" id="[^"]*" value="\K[^"]*')
 # Check if ntok is valid - 40 hex characters
 if ! [[ -n "$NTOK" ]]
 then
-    LOG_MESSAGE "Couldn't find ntok for $MON_TMPL_NAME Name: $NODE_NAME IP: $IP Port: $PORT" "err"
+    LOG_MESSAGE "Couldn't find ntok" "err"
     exit 1
 fi
 
 # Log in with the cookie, ntok, username, and password
-HTTP_CODE=$(curl -k -b "$COOKIE_FILE" -w "%{http_code}" -d "target=&ntok=$NTOK&static_u=&no_u=&no_p=&username=$USERNAME&F_password=0&password=$PASSWORD" "$URL" -s -S -o /dev/null)
+CURL_DATA="target=&ntok=$NTOK&static_u=&no_u=&no_p=&username=$USERNAME&F_password=0&password=$PASSWORD"
+HTTP_CODE=$(curl -k -b "$COOKIE_FILE" -w "%{http_code}" -d $CURL_DATA "$URL" -s -S -o /dev/null)
 
 # Expecting a 302 redirect upon successful login
 if [[ $HTTP_CODE -eq 302 ]]
@@ -146,18 +152,18 @@ then
     # Warn if logout fails, but don't quit
     if ! [[ $HTTP_CODE -eq 302 ]]
     then
-        LOG_MESSAGE "$MON_TMPL_NAME - PID: $$ Name: $NODE_NAME IP: $IP Port: $PORT failed to logout." "warn"
+        LOG_MESSAGE "Failed to logout" "warn"
     fi
 
     END_TIME=$(date +%s%N)
     ELAPSED_TIME=$(awk "BEGIN {print ($END_TIME - $START_TIME) / 1000000}")
-    LOG_MESSAGE "$MON_TMPL_NAME - PID: $$ Name: $NODE_NAME IP: $IP Port: $PORT Succeeded after $ELAPSED_TIME ms" "info"
+    LOG_MESSAGE "Succeeded after $ELAPSED_TIME ms" "info"
 
     CLEANUP
     
     echo UP
     exit 0
 else
-    LOG_MESSAGE "$MON_TMPL_NAME - PID: $$ Name: $NODE_NAME IP: $IP Port: $PORT Incorrect username or password" "err"
+    LOG_MESSAGE "Incorrect username or password, or bad decryption key" "err"
     exit 1
 fi
